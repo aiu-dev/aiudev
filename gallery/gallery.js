9ec76cd283013cc3f4c5d305d9bfb90d11e72a0a
aiu-dev <abubakaribrahimumar806@gmail.com>
2026-07-06 14:59:17 +0100

    adding 6 pages

---
+++ gallery/gallery.js
@@ -0,0 +1,23 @@
+
+const galleryContainer = document.querySelector(".gallery-container");
+const galleryObject = [
+     {id:5,src:"../images/mylogo.png"},,
+     {id:1,src:"../images/gallery3.png"},
+     {id:2,src:"../images/gallery2.png"},
+     {id:3,src:"../images/gallery4.png"},
+     {id:4,src:"../images/gallery5.png"},
+     {id:6,src:"../images/profile.png"},
+
+];
+
+    function renderPhoto(){
+    galleryContainer.innerHTML = "";
+     galleryObject.forEach((photo) =>{
+      const html= `<img alt="my picture"  height="360px"  width="300px" class="photo" src="${photo.src}">`
+     const div = document.createElement("div");
+      div.innerHTML = html;
+      galleryContainer.appendChild(div);
+})
+}
+//display gallery function
+renderPhoto();
--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 const navBar = document.getElementById("navBar");
 const displayNav = document.getElementById("nav");
 const cancelNav = document.getElementById("cancel-btn");
-const overlay = document.querySelectorAll(".section")
-navBar.addEventListener("click", () =>{
+const overlay = document.querySelectorAll(".section");
+
+navBar.addEventListener("click", () => {
     displayNav.style.display = "flex";
 });
-cancelNav.addEventListener("click", () => {
-    displayNav.style.display="none";
-});
-for(let i =0; i<=overlay.length;i ++){
-overlay[i].addEventListener("click", ()=>{
-  if(displayNav.style.display="flex"){
-      displayNav.style.display="none";
-  }  
-});
-}
 
+cancelNav.addEventListener("click", () => {
+    displayNav.style.display = "none";
+});
+
+overlay.forEach(section => {
+    section.addEventListener("click", () => {
+        if (displayNav.style.display === "flex") {
+            displayNav.style.display = "none";
+        }
+    });
+});
