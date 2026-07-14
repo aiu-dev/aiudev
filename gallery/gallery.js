1f8f6db6b453d75431c96e713ecdda893d3d2908
aiu-dev <abubakaribrahimumar806@gmail.com>
2026-07-14 16:39:39 +0100

    updating gallery.js

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -1,12 +1,12 @@
 
 const galleryContainer = document.querySelector(".gallery-container");
 const galleryObject = [
-     {id:5,src:"../images/mylogo.png"},,
+     {id:5,src:"../images/mylogo.png"},
      {id:1,src:"../images/gallery3.png"},
      {id:2,src:"../images/gallery2.png"},
      {id:3,src:"../images/gallery4.png"},
      {id:4,src:"../images/gallery5.png"},
-     {id:6,src:"../images/profile.png"},
+     {id:6,src:"../images/profile.png"}
 
 ];
 
