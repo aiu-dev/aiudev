60783497e1f8ccb2e80677a137cec39ee3d11759
aiu-dev <abubakaribrahimumar806@gmail.com>
2026-07-14 18:40:42 +0100

    gallery render issue

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -4,12 +4,12 @@ const galleryContainer = document.querySelector(".gallery-container");
 console.log("Gallery container:", galleryContainer);
 
 const galleryObject = [
-  { id: 5, src: "../Images/mylogo.png" },
-  { id: 1, src: "../Images/gallery3.png" },
-  { id: 2, src: "../Images/gallery2.png" },
-  { id: 3, src: "../Images/gallery4.png" },
-  { id: 4, src: "../Images/gallery5.png" },
-  { id: 6, src: "../Images/profile.png" }
+  { id: 5, src: "../images/mylogo.png" },
+  { id: 1, src: "../images/gallery3.png" },
+  { id: 2, src: "../images/gallery2.png" },
+  { id: 3, src: "../images/gallery4.png" },
+  { id: 4, src: "../images/gallery5.png" },
+  { id: 6, src: "../images/profile.png" }
 ];
 
 function renderPhoto() {
