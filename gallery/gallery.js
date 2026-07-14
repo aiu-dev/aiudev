d27c24adf050432f85831275433d5e8a05086463
aiu-dev <abubakaribrahimumar806@gmail.com>
2026-07-14 18:12:57 +0100

    gallery issue

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -1,23 +1,33 @@
 
 const galleryContainer = document.querySelector(".gallery-container");
-const galleryObject = [
-     {id:5,src:"../images/mylogo.png"},
-     {id:1,src:"../images/gallery3.png"},
-     {id:2,src:"../images/gallery2.png"},
-     {id:3,src:"../images/gallery4.png"},
-     {id:4,src:"../images/gallery5.png"},
-     {id:6,src:"../images/profile.png"}
 
+console.log("Gallery container:", galleryContainer);
+
+const galleryObject = [
+  { id: 5, src: "../Images/mylogo.png" },
+  { id: 1, src: "../Images/gallery3.png" },
+  { id: 2, src: "../Images/gallery2.png" },
+  { id: 3, src: "../Images/gallery4.png" },
+  { id: 4, src: "../Images/gallery5.png" },
+  { id: 6, src: "../Images/profile.png" }
 ];
 
-    function renderPhoto(){
-    galleryContainer.innerHTML = "";
-     galleryObject.forEach((photo) =>{
-      const html= `<img alt="my picture"  height="360px"  width="300px" class="photo" src="${photo.src}">`
-     const div = document.createElement("div");
-      div.innerHTML = html;
-      galleryContainer.appendChild(div);
-})
+function renderPhoto() {
+  if (!galleryContainer) {
+    alert("Gallery container not found");
+    return;
+  }
+
+  galleryObject.forEach(photo => {
+    const img = document.createElement("img");
+    img.src = photo.src;
+    img.alt = "My picture";
+    img.width = 300;
+    img.height = 360;
+    img.className = "photo";
+
+    galleryContainer.appendChild(img);
+  });
 }
-//display gallery function
+
 renderPhoto();
